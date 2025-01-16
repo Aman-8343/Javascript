0834d4faecfd58278927eaab0254c0e69232d1c6
Aman kumar <amankumar105541@gmail.com>
2025-01-16 10:09:25 +0530

    Add files via upload

---
+++ function/function.js
@@ -0,0 +1,124 @@
+// function yo(msg){
+//     console.log(msg );
+// }
+// yo("javascript");
+
+
+//sum of number
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(3,6);
+
+// function sum(a,b){
+//     s=a+b;
+//     console.log(a);
+//     return s;
+// }
+//  let val= sum(2,7);
+//  console.log(val);
+
+// const sum=(a,b)=>{
+//     return a+b;
+// }
+//  let val =sum(2,4)
+//  console.log(val)
+
+
+//  const arrowmul =(a,b)=>{
+//     console.log(a*b)
+//  }
+
+// function countVowels(str){
+//     let count=0;
+// for(let char of str){
+//     console.log (char);
+//     if(char==="a"||char ==="e"|| char ==="i"|| char==="o"|| char==="u"){
+//         count++;
+//     }
+// }
+// return count;
+// }
+
+
+// let countvowels =(str) =>{
+//     let count=0;
+//     for(let char of str){
+        
+//         if(char==="a"||char ==="e"|| char ==="i"|| char==="o"|| char==="u"){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+
+// for each method
+
+// let arr=[1,2,3,4,5];
+// arr.forEach(function printVal(val){
+// console.log(val)
+// } )
+
+let arr=[1,2,3,4,5];
+arr.forEach((val ,idx,arr) =>{
+    console.log(val,idx,arr);
+}
+)
+
+
+// let arr=[1,2,3,4,5];
+// arr.forEach(( arr) =>{
+//   console.log( "array is" ,arr);
+//   console.log ("square of array is",arr*arr)
+
+// }
+// )
+
+
+// let array =[34,45,3,8,5,43];
+//  let newArray =array.map((val) =>{
+//    return val*val;
+
+// })
+// console.log(newArray)
+
+// let arr=[1,2,3,4,5,6];
+// let newArray =arr.filter((val) => {
+//     return val %2===0;
+// })
+// console.log(newArray);
+
+//  let arr=[1,2,3,4]
+//  let output=arr.reduce((prev,curr) =>{
+//     return prev>curr ? prev:curr ;
+// })
+// console.log(output)
+
+//practice question
+
+// let marks=[91,67,34,90,93]
+//  let student=marks.filter(( val) =>{
+//     return val>90;
+// })
+// console.log(student)
+
+
+// let n = prompt("Enter a number");
+// let arr =[];
+// for (let i=0;i<=n;i++){
+//     arr[i-1]=i;
+// }
+// console.log(arr);
+
+//  let sum =arr.reduce((prev,res)=>{
+//     return prev+res;
+// })
+// console.log("sum of array=", sum)
+
+// let factorial =arr.reduce((prev,res)=>{
+//     return prev*res;
+// })
+// console.log("factorial of array=", factorial)
---
+++ loops/loops.js
@@ -0,0 +1,58 @@
+// for (a=1; a<=10;a++){
+//     console.log("aman");
+// }
+
+// let sum=0;
+
+// for (i=1;i<=5;i++){
+//     sum=sum+i;
+// }
+// console.log("sum=", +sum);
+
+
+
+// let b=1;
+// while(b<=15){
+//     console.log("aaa");
+//     b++;
+// }
+
+// let c=1;
+// do{
+//     console.log("aaa");
+//     c++;
+// }while(c<=10);
+
+// let a=1;
+// do{
+//     console.log("a=",a)
+//     a++
+// }while (a<=10);
+
+
+let size=0;
+let string="amankumararya javascript";
+for(let i of string){
+    console.log("i=",i)
+    size++;
+}
+console.log("string size = ", size);
+
+// 
+
+// for(i=0;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+   
+// }                                   //even number
+
+
+// let str="javascript";
+// console.log(str[2]);
+
+// let string=`this is template literals \n $(1 + 2 + 3)`;
+// console.log(string);
+
+let str="aman";
+console.log(str.toUpperCase())
