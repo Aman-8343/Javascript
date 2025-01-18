d55dc23432621b3914a5e0ab0ce3eac0f07b1eb4
Aman kumar <amankumar105541@gmail.com>
2025-01-18 09:08:54 +0530

    Add files via upload

---
+++ dom/script.js
@@ -0,0 +1,27 @@
+console.log ("gfgjjghj");
+window.console.log("hello");
+console.log(window);
+console.dir(window);
+console.dir(document.body);
+console.log(document.body);
+
+
+ let heading=document.getElementById("box");
+ console.log(heading);
+console.dir(heading)
+
+document.querySelector("para");
+
+
+let div=document.querySelector("div");
+console.dir(div)
+
+let heading=document.querySelector("h1");
+
+ let div=document.querySelector("div");
+//  console.log(div);
+//  let id= div.setAttribute("id","newid");
+//  console.log(id)
+
+div.style.backgroundColor="purple";
+
